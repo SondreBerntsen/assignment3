c8624a0c1044a3634ff407218e31ad443ad9bf2e
ragnals <ragnals@stud.ntnu.no>
2018-05-31 15:24:34 +0200

    category functions

--- model/Category.php
+++ model/Category.php
@@ -1,19 +1,29 @@
 <?php
 // Class for Category
+include('model/model.php');
+class Category extends Model {
 
-class Category {
-	public $title;
-
-	public function __construct($title){
-       $this->title = $title;
-  }
-	public function createCategory(){
-
+	$db = $this->connectToDB();
+//createCategory function creates a new category
+	public function createCategory($name){
+		$query = 'INSERT INTO category (name) VALUES (?)';
+    $sth = $this->db->prepare($query);
+    $data = array($name);
+   $sth->execute($data);
+   //If there is a new row in a database retrun true
+   if ($sth->rowCount() == 1){
+     return true;
+   }else{//return false, could not insert category in db
+     return false;
+   }
 	}
-	public function delteCategory(){
-		//Om vi har tid, trenger ikk prioriteres
+	public function delteCategory($name){
+		$db = $this->connectToDB();
+
+		$query = 'DELETE FROM category WHERE name=?';
+    $sth = $this->db->prepare($query);
+    $sth->execute(array($name));
 	}
-	
 }
 
 ?>
