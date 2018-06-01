ef7329b7e781ed79e6342e9a67f71aa7ac022530
elissi <elisamed@stud.ntnu.no>
2018-06-01 13:17:38 +0200

    deleted the view for register, since it is now a module

--- model/model.php
+++ model/model.php
@@ -1,19 +1,12 @@
 <?php
 
-//include_once("model/Category.php");
+include_once("model/Category.php");
 /*include_once("model/Item.php");
 include_once("model/User.php");
 include_once("model/MessageThread.php");
 include_once("model/Message.php");*/
 
 class Model {
-	public function connectToDB(){
-		$dsn= 'mysql:dbname=secondhand;host=127.0.0.1';
-		$user = 'root';
-		$password = '';
-		$db = new PDO($dsn, $user, $password);
-		return $db;
-	}
 	public function getCategoryList()
 	{
 		// here goes some hardcoded values to simulate the database
@@ -24,10 +17,12 @@ class Model {
 		);
 	}
 
-
-
-	public function newItem(){
-
+	public function getCategory($title)
+	{
+		// we use the previous function to get all the books and then we return the requested one.
+		// in a real life scenario this will be done through a db select command
+		$allCategories = $this->getCategoryList();
+		return $allCategories[$title];
 	}
 
 
