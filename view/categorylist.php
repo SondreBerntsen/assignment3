e54905029e1fb0d48b9d2dfd6bd97b45f3bcf50a
sondre <sondre_berntsen94@hotmail.com>
2018-06-01 12:58:35 +0200

    fixed cards on categorylist view. fixed structure of divs from hard css to bootstrap grid system.

--- view/categorylist.php
+++ view/categorylist.php
@@ -3,23 +3,58 @@ include_once '../includes/header.inc.php';
  ?>
 
 	<!-- sidebar for categories -->
-	<div class="sidenav">
-		<ul>
-			<li>
-				<h4>
-					<strong>Kategorier</strong>
-				</h4>
-			</li>
-			<?php
-			// for every category in categories
-			foreach ($categories as $title => $category)
-			{
-			// we place the category title in the url
-			echo '<li> <a href="index.php?category='.$category->title.'">'.$category->title.'</a></li>';
-			}
-			?>
-		</ul>
-	</div>
+	<div class="container">
+  <div class="row">
+    <div class="col-sm-3">
+			<div class="sidenav">
+				<ul>
+					<li>
+						<h4>
+							<strong>Kategorier</strong>
+						</h4>
+					</li>
+					<?php
+					// for every category in categories
+					foreach ($categories as $title => $category)
+					{
+					// we place the category title in the url
+					echo '<li> <a href="index.php?category='.$category->title.'">'.$category->title.'</a></li>';
+					}
+					?>
+				</ul>
+			</div>
+    </div>
+    <div class="col">
+			<div class="card-group">
+			  <div class="card">
+			    <img class="card-img-top" src="../resources/images/testimg.jpg" alt="Card image cap">
+			    <div class="card-body">
+			      <h5 class="card-title">Card title</h5>
+			      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida dapibus vestibulum.</p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+				  </div>
+				  <div class="card">
+				    <img class="card-img-top" src="../resources/images/testimg.jpg" alt="Card image cap">
+				    <div class="card-body">
+				      <h5 class="card-title">Card title</h5>
+				      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida dapibus vestibulum.</p>
+				      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				    </div>
+				  </div>
+				  <div class="card">
+				    <img class="card-img-top" src="../resources/images/testimg.jpg" alt="Card image cap">
+				    <div class="card-body">
+				      <h5 class="card-title">Card title</h5>
+				      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida dapibus vestibulum.</p>
+				      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				    </div>
+				  </div>
+			</div>
+    </div>
+  </div>
+</div>
+
 
 	<?php
 
