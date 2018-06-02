7f001b1911ccaf93b63cddcc7c344afa91908147
sondre <sondre_berntsen94@hotmail.com>
2018-06-02 13:55:56 +0200

    index footer and header include

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 	include_once("controller/HomeController.php");
+	include_once("includes/header.inc.php");
 
 	$controller = new HomeController();
 	$controller->listCategories();
+
+	include_once("includes/footer.inc.php");
 
 ?>
