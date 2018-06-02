c32cbc58d5e452da5e11ad37dd93bd539d8449fb
ragnals <ragnals@stud.ntnu.no>
2018-06-02 16:43:52 +0200

    item card fix

--- view/home.php
+++ view/home.php
@@ -13,15 +13,15 @@
 			<section>
 			    <div class="card">
 			      <div class="row">
-			        <div class="col-md-6">
+			        <div class="col-md-8">
 			          <div class="card-block">
 			            <h5 class="card-title">Curabitur gravida vestibulum imperdiet.</h5>
 									<h6 class='card-subtitle mb-2 text-muted'>02.06.2018</h6><br />
 			            <p class="card-text">Cras convallis ut turpis vitae facilisis. Morbi eu augue vel quam efficitur rhoncus vitae eget lectus. Cras augue ligula, aliquam ut enim ut, feugiat imperdiet sem. Integer sed mi quis nisl eleifend interdum.</p>
 			          </div>
 			        </div>
-			        <div class="col-md-6">
-			          <div class="card-img-bottom">
+			        <div class="col-md-4">
+			          <div class="card-img-bottom float-right">
 									<img src="resources/images/testimg.jpg" class="img-fluid imgItem" alt="Responsive image">
 			          </div>
 			        </div>
