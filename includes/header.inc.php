f9a21f5342bec5b05232f6a7d5c5e89da557d816
sondre <sondre_berntsen94@hotmail.com>
2018-06-02 15:01:57 +0200

    fixed css links yet again

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -3,7 +3,7 @@
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="../styles/style.css">
+	<link rel="stylesheet" href="styles/style.css">
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
