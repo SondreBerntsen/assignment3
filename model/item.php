f03d9e751a07d3087d09f9138c1df47cf8d0c332
ragnals <ragnals@stud.ntnu.no>
2018-06-02 12:03:52 +0200

    item functions

--- model/item.php
+++ model/item.php
@@ -1,6 +1,6 @@
 <?php
-
-class Item {
+require_once('model/model.php');
+class Item extends Model  {
 
 
 	public function __construct()
@@ -8,14 +8,40 @@ class Item {
 
     }
 
-	public function createItem(){
+	public function newItem($values){
+		$db = $this->connectToDB();
 
-	}
-	public function getItem(){
+		$query = 'INSERT INTO item(name, descr, img, date, owner, category, previewtxt)
+							VALUES (?, ?, ?, NOW(), ?, ?, ?)';
+		$sth = $db->prepare($query);
+		$sth->execute([$values[0], $values[1], $values[2], $_SESSION['user'], $values[3], $values[4]]);
 
-	}
-	public function deleteItem(){
+		if($sth->rowCount() == 1){
+			return "Things were inserted";
+		}else{
+			return "No workerino";
+		}
+  }
+	public function getItem($catName){
+		$db = $this->connectToDB();
+		$query = 'SELECT *
+							FROM item
+							WHERE category = ?
+							ORDER BY date DESC';
 
+		$sth = $db->prepare($query);
+		$data = array($catName);
+		$sth->execute($data);
+		$itemData=$sth->fetchAll(PDO::FETCH_ASSOC);
+		return $itemData;
+
+		}
+	public function deleteItem($itemId){
+		$db = $this->connectToDB();
+
+		$query = 'DELETE FROM item WHERE id=?';
+		$sth = $db->prepare($query);
+		$sth->execute(array($itemId));
 	}
 	public function editItem(){
 
