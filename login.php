55daeb5382e564c363e93236e3e386953b3d068a
ragnals <ragnals@stud.ntnu.no>
2018-06-03 18:10:46 +0200

    login.php

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+	include_once("includes/header.inc.php");
+?>
+
+<!-- Different views are loaded here -->
+<?php
+	include('view/login.php');
+	include_once("includes/homeFooter.inc.php");
+
+?>
