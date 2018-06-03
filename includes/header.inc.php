915c216651376671239bdb613077a39140d4bd7e
sondre <sondre_berntsen94@hotmail.com>
2018-06-03 15:57:24 +0200

    register and login view NEW. some header changes nothing major, btw the linking in nav is messed up

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -8,13 +8,15 @@
 	<link rel="stylesheet" href="styles/style.css">
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<!-- Fontawesome icons -->
+	<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
 </head>
 
 <body>
 
   <!-- navbar -->
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<a class="navbar-brand" href="index.php">Secondhand</a>
+		<a class="navbar-brand" href="view/home.php">Secondhand</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
@@ -22,70 +24,80 @@
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item">
-					<a class="nav-link" href="categorylist.php">Home</a>
+					<a class="nav-link" href="view/home.php">Home</a>
 				</li>
 			</ul>
 		</div>
 
-		<!-- Saving the logout button for when the database is up and running.
+		<a href="view/login.php" class="btn btn-success" role="button">
+			<i class="fas fa-user"></i> Login
+		</a>
+
+		</nav>
+
+		<!-- Modal button and form
 
 		<form class="form-inline my-2 my-lg-0" action="includes/logout.inc.php" method="POST">
 			<button class="btn btn-outline-danger my-2 my-sm-0" type="submit" name="submit">Logout</button>
-		</form>-->
+		</form>
 
 		<form class="form-inline my-2 my-lg-0" action="includes/login.inc.php" method="POST">
 			<input name="username" class="form-control mr-sm-2" type="text" placeholder="username" aria-label="username">
 			<input name="password" class="form-control mr-sm-2" type="password" placeholder="password" aria-label="password">
 		<button class="btn btn-success marginbutton" type="submit" name="submit">Login</button>
-		<!-- Button trigger modal -->
+		-->
+
+		<!-- Button trigger modal
 		<button type="button" class="btn btn-outline-info marginbutton" data-toggle="modal" data-target="#registerModal">
-			Register
-		</button>
+				Register
+			</button>
 		</form>
-	</nav>
-  <!-- Modal -->
-  <div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Register yourself!</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-					<!--We need to write the action script for registering a user -->
-					<form  class="register" action="register.php" method="post">
-		        <div class="form-group row">
-		          <label for="firstName" class="col-sm-2 col-form-label">Fornavn</label>
-		          <div class="col-sm-10">
-		            <input type="text" class="form-control" id="firstName" name="firstName" autofocus required placeholder="Fornavn">
-		          </div>
-		        </div>
-		        <div class="form-group row">
-		          <label for="lastName" class="col-sm-2 col-form-label">Etternavn</label>
-		          <div class="col-sm-10">
-		            <input type="text" class="form-control" id="lastName" name="lastName" required placeholder="Etternavn">
-		          </div>
-		        </div>
-		        <div class="form-group row">
-		          <label for="email" class="col-sm-2 col-form-label">E-post</label>
-		          <div class="col-sm-10">
-		            <input type="email" class="form-control" id="email" name="email" required placeholder="E-post">
-		          </div>
-		        </div>
-		        <div class="form-group row">
-		          <label for="password" class="col-sm-2 col-form-label">Passord</label>
-		          <div class="col-sm-10">
-		            <input type="password" class="form-control" id="pwd" name="pwd" required placeholder="*******">
-		          </div>
-		        </div>
-						<div class="modal-footer">
-							<button type="submit" class="btn btn-primary">Register user</button>
-							<button type="button" class="btn btn-secondary" data-dismiss="modal">...Nevermind!</button>
-						</div>
-		      </form>
-        </div>
-      </div>
-    </div>
-  </div>
+		 -->
+
+	  <!-- Modal
+	  <div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	    <div class="modal-dialog" role="document">
+	      <div class="modal-content">
+	        <div class="modal-header">
+	          <h5 class="modal-title" id="exampleModalLabel">Register yourself!</h5>
+	          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	            <span aria-hidden="true">&times;</span>
+	          </button>
+	        </div>
+	        <div class="modal-body">
+					-->
+						<!--We need to write the action script for registering a user -->
+						<!--<form  class="register" action="register.php" method="post">
+			        <div class="form-group row">
+			          <label for="firstName" class="col-sm-2 col-form-label">Fornavn</label>
+			          <div class="col-sm-10">
+			            <input type="text" class="form-control" id="firstName" name="firstName" autofocus required placeholder="Fornavn">
+			          </div>
+			        </div>
+			        <div class="form-group row">
+			          <label for="lastName" class="col-sm-2 col-form-label">Etternavn</label>
+			          <div class="col-sm-10">
+			            <input type="text" class="form-control" id="lastName" name="lastName" required placeholder="Etternavn">
+			          </div>
+			        </div>
+			        <div class="form-group row">
+			          <label for="email" class="col-sm-2 col-form-label">E-post</label>
+			          <div class="col-sm-10">
+			            <input type="email" class="form-control" id="email" name="email" required placeholder="E-post">
+			          </div>
+			        </div>
+			        <div class="form-group row">
+			          <label for="password" class="col-sm-2 col-form-label">Passord</label>
+			          <div class="col-sm-10">
+			            <input type="password" class="form-control" id="pwd" name="pwd" required placeholder="*******">
+			          </div>
+			        </div>
+							<div class="modal-footer">
+								<button type="submit" class="btn btn-primary">Register user</button>
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">...Nevermind!</button>
+							</div>
+			      </form>
+	        </div>
+	      </div>
+	    </div>
+	  </div> -->
---
+++ view/login.php
@@ -0,0 +1,81 @@
+<?php
+include '../includes/header.inc.php'
+ ?>
+
+ <div class="container">
+   <div class="row">
+     <div class="col-7">
+       <!-- register form-->
+       <form  class="register" action="register.php" method="post">
+         <div class="form-group row">
+           <div class="col-sm-12">
+            <label for="firstName" class="form-label">First name</label>
+             <input type="text" class="form-control" id="firstName" name="firstName" autofocus required placeholder="First name">
+           </div>
+         </div>
+         <div class="form-group row">
+           <div class="col-sm-12">
+             <label for="lastName" class="form-label">Last name</label>
+             <input type="text" class="form-control" id="lastName" name="lastName" required placeholder="Last name">
+           </div>
+         </div>
+         <div class="form-group row">
+           <div class="col-sm-12">
+             <label for="email" class="form-label">E-mail</label>
+             <input type="email" class="form-control" id="email" name="email" required placeholder="E-mail">
+           </div>
+         </div>
+         <div class="form-group row">
+           <div class="col-sm-12">
+             <label for="password" class="form-label">Password</label>
+             <input type="password" class="form-control" id="pwd" name="pwd" required placeholder="*******">
+           </div>
+         </div>
+           <button name="Mads sin knapp" type="submit" class="btn btn-primary">Register user</button>
+       </form>
+     </div>
+
+
+     <div class="col-sm">
+      <!--login form -->
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Login</h1>
+          <form action="login.php" method="post">
+            <div class="form-group row">
+              <div class="col-sm-12">
+               <label for="firstName" class="form-label">Email</label>
+                <input type="text" class="form-control" id="firstName" name="email" autofocus required placeholder="email">
+              </div>
+            </div>
+
+            <div class="form-group row">
+              <div class="col-sm-12">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="pwd" name="pwd" required placeholder="*******">
+              </div>
+            </div>
+              <button name="Mads sin knapp" type="submit" class="btn btn-success">Login!</button>
+          </form>
+        </div>
+      </div>
+
+     </div>
+   </div>
+ </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <?php
+ include '../includes/homeFooter.inc.php'
+  ?>
