c410b0793ff8670ea0d4d5b65937fc8786a2851a
sondre <sondre_berntsen94@hotmail.com>
2018-06-04 12:59:38 +0200

    user profile page view with tabs for content and stuff

---
+++ view/user.php
@@ -0,0 +1,87 @@
+<?php
+include '../includes/header.inc.php';
+ ?>
+ <!--
+<div class="container">
+  <div class="btn-group btn-group-toggle" data-toggle="buttons">
+   <label class="btn btn-secondary">
+     <input type="radio" name="options" id="option1" autocomplete="off"> Listings
+   </label>
+   <label class="btn btn-secondary">
+     <input type="radio" name="options" id="option2" autocomplete="off"> Conversations
+   </label>
+   <label class="btn btn-secondary active">
+     <input type="radio" name="options" id="option3" autocomplete="off"> Settings
+   </label>
+  </div>
+
+
+<div class="container">
+  <div class="col-6">
+    <div class="list-group" id="myList" role="tablist">
+      <a class="list-group-item list-group-item-action active" data-toggle="list" href="#settings" role="tab">Settings</a>
+      <a class="list-group-item list-group-item-action" data-toggle="list" href="#listings" role="tab">Listings</a>
+      <a class="list-group-item list-group-item-action" data-toggle="list" href="#messages" role="tab">Messages</a>
+    </div>
+  </div>
+
+  <div class="col-6">
+      <div class="tab-content">
+        <div class="tab-pane active" id="settings" role="tabpanel">Here goes the settings </div>
+        <div class="tab-pane" id="listings" role="tabpanel">Here goes the listeroonies</div>
+        <div class="tab-pane" id="messages" role="tabpanel">Sliding into these DMs</div>
+      </div>
+  </div>
+</div>
+-->
+
+<div class="container py-3">
+    <div class="row">
+        <div class="col-10 mx-auto">
+          <div class="profileheader">
+            <h2 class="mb-3 profileheading justify-content-left">Your profile</h2>
+          </div>
+            <ul class="nav nav-tabs small justify-content-end" role="tablist">
+                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#settings" role="tab">Settings</a></li>
+                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#listings" role="tab">Listings</a></li>
+                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#messages" role="tab">Messages</a></li>
+            </ul>
+            <div id="content" class="tab-content py-4">
+                <div class="tab-pane active" id="settings" role="tabpanel">
+                  <h4 class="mb-3">Change your settings</h2>
+                  <p>The nav-tabs above use `justify-content-end` to align all tabs on the right. Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut
+                      semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave
+                      meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+                </div>
+                <div class="tab-pane" id="listings" role="tabpanel">
+                  <h4 class="mb-3">Listings</h2>
+                  <p>The nav-tabs above use `justify-content-end` to align all tabs on the right. Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut
+                      semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave
+                      meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+                </div>
+                <div class="tab-pane" id="messages" role="tabpanel">
+                  <h4 class="mb-3">Messages</h2>
+                  <p>The nav-tabs above use `justify-content-end` to align all tabs on the right. Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut
+                      semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave
+                      meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <?php
+ include '../includes/homeFooter.inc.php';
+  ?>
