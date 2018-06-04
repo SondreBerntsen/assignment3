c80bf1571cf8ab5dfcf3800992b9a60a2eb73a22
sondre <sondre_berntsen94@hotmail.com>
2018-06-04 16:06:29 +0200

    headerchanges linking and such

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -30,9 +30,6 @@ session_start();
 				<li class="nav-item">
 					<a class="nav-link" href="index.php">Home</a>
 				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="login.php">Register</a>
-				</li>
 			</ul>
 		</div>
 
