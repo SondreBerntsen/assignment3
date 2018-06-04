c58157419ccf07de81654a695234202eba0b123b
sondre <sondre_berntsen94@hotmail.com>
2018-06-04 11:18:12 +0200

    changed the name of the login button

--- view/login.php
+++ view/login.php
@@ -55,7 +55,7 @@ include '../includes/header.inc.php'
                 <input type="password" class="form-control" id="pwd" name="pwd" required placeholder="*******">
               </div>
             </div>
-              <button name="Mads sin knapp" type="submit" class="btn btn-success">Login!</button>
+              <button name="Mads sin knapp2" type="submit" class="btn btn-success">Login!</button>
           </form>
         </div>
       </div>
