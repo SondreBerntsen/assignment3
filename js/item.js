87f717748afc372a4144f36f2199b54db2753c78
mads <mmoigaar@stud.ntnu.no>
2018-06-05 18:12:18 +0200

    item maybe

--- js/item.js
+++ js/item.js
@@ -34,20 +34,10 @@ function checkExistingThread(){
       item: itemID
     },
     type: 'post',
-           success: function(output) {
-             json = JSON.parse(output);
-             console.log(json);
-
-               var tmpl = $('#tmplItem').clone();
-               tmpl.removeAttr('id');
-               tmpl.find('.itemName').html(json.name);
-               tmpl.find('.itemDate').html(json.date);
-               tmpl.find('.itemDescr').html(json.descr);
-               tmpl.find('.nameOwner').html(json.firstName +" "+ json.lastName);
-
-               $('#itemContainer').append(tmpl);
-
-           }
+    success: function(output) {
+      json = JSON.parse(output);
+      console.log(json);
+     }
   });
 
 }
