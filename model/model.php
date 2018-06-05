33015e21ae54545edbb202bbd8bba4c381d82078
ragnals <ragnals@stud.ntnu.no>
2018-06-05 11:33:33 +0200

    deleted som shit

--- model/model.php
+++ model/model.php
@@ -8,24 +8,6 @@ class Model {
 			 $db = new PDO($dsn, $user, $password);
 			 return $db;
 	 }
-	public function getCategoryList()
-	{
-		// here goes some hardcoded values to simulate the database fix
-		return array(
-			"category1" => new Category("category1"),
-			"category2" => new Category("category2"),
-			"category3" => new Category("category3")
-		);
-	}
-
-	public function getCategory($title)
-	{
-		// we use the previous function to get all the books and then we return the requested one.
-		// in a real life scenario this will be done through a db select command
-		$allCategories = $this->getCategoryList();
-		return $allCategories[$title];
-	}
-
 
 }
 
