e34e105e84dc7ceebdd88492d54a4e171424b3a6
sondre <sondre_berntsen94@hotmail.com>
2018-06-05 18:12:40 +0200

    message modal

--- view/item.php
+++ view/item.php
@@ -26,33 +26,19 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="sendMessageModalTitle">Your message</h5>
+            <h5 class="itemTitle">Item in question</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div class="modal-body">
+            <h6 class="modal-title" id="sendMessageModalTitle">Your message</h6>
             <form action="includes/entries.inc.php" method="POST">
               <div class="form-group">
-                <label for="itemTitle">Item in question</label>
-                <input type="text" class="form-control" placeholder="Enter entry title" name="title">
-              </div>
-              <div class="form-group">
                 <textarea class="form-control" name="content" rows="5"></textarea>
-                <small id="usernameHelp" class="form-text text-muted">Hope your new entry is a hit!</small>
-              </div>
-              <div class="form-group">
-                <label for="exampleFormControlSelect1">Select topic</label>
-                <select class="form-control" name="topics">
-                  <?php
-                      while ($row = mysqli_fetch_array($result)) {
-                          echo "<option name='topics' value='" . $row['topic_id'] . "'>" . $row['topic_name'] . "</option>";
-                      }
-                  ?>
-                </select>
+                <small class="form-text text-muted">You will be taken to your message thread after sending the message!</small>
               </div>
               <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-
             </form>
           </div>
         </div>
