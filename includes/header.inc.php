e1c0b8c48033758be1d98dbb69346de03a3371f0
elissi <elisamed@stud.ntnu.no>
2018-06-06 13:26:30 +0200

    added font

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -11,6 +11,7 @@ session_start();
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 	<link rel="stylesheet" href="styles/style.css">
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
 
 	<!-- Fontawesome icons -->
 	<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
@@ -34,7 +35,5 @@ session_start();
 		</div>
 		<div id="headerButton">
 		</div>
-
-
 
 		</nav>
