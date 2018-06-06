f3973575121549b2b607e210c5798c7fee88c835
elissi <elisamed@stud.ntnu.no>
2018-06-06 15:05:42 +0200

    fonts

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -11,7 +11,7 @@ session_start();
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 	<link rel="stylesheet" href="styles/style.css">
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
-	<link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poiret+One|Montserrat:300|" rel="stylesheet">
 
 	<!-- Fontawesome icons -->
 	<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
