d5408bfc337bf6540d42f9d11ada805003ce0c83
mads <mmoigaar@stud.ntnu.no>
2018-06-07 23:00:21 +0200

    H E L V E T E S A T A N

--- js/uploadItem.js
+++ js/uploadItem.js
@@ -1,3 +1,21 @@
+$(document).ready(function (){
+  checkLoginState();
+  $.ajax({
+    url: 'controller/uploadItemController.php',
+    data: {action: 'listCategories'},
+    type: 'post',
+    success: function(output) {
+      json = JSON.parse(output);
+      for(i=0; i < json.length; i++){
+        var tmpl = $('#catTempl').clone();
+        tmpl.removeAttr('id');
+        tmpl.html(json[i].name);
+        tmpl.attr('value', json[i].name);
+        $('#selectCategory').append(tmpl);
+      }
+    }
+  });
+});
 
 function submitItem (){
   var name = itemName.value;
@@ -18,32 +36,31 @@ if($('#itemName').val()==""||$('#itemPreview').val()==""||$('#itemDescr').val()=
     });
 } else {
 
-  var data = new FormData();
-  jQuery.each(jQuery('#uploadimg')[0].files, function(i, file) {
-    data.append('imgfile', file);
-    data.append('name', name);
-    data.append('preview', preview);
-    data.append('descr', descr);
-    data.append('category', category);
-  });
+    var data = new FormData();
+    jQuery.each(jQuery('#uploadimg')[0].files, function(i, file) {
+      data.append('imgfile', file);
+      data.append('name', name);
+      data.append('preview', preview);
+      data.append('descr', descr);
+      data.append('category', category);
+    });
 
-  $.ajax( {
-    url: 'controller/uploadItemController.php',
-    data: data,
-    cache: false,
-    contentType: false,
-    processData: false,
-    method: 'POST',
-    type: 'POST', // For jQuery < 1.9
-    success: function(data, output){
-      console.log(data);
-      json = output;
-      console.log(json);
-    }
-  });
+    $.ajax( {
+      url: 'controller/uploadItemController.php',
+      data: data,
+      cache: false,
+      contentType: false,
+      processData: false,
+      method: 'POST',
+      type: 'POST', // For jQuery < 1.9
+      success: function(data, output){
+        console.log(data);
+        json = output;
+        console.log(json);
+      }
+    });
 
-
-}
+  }
 }
 
   //https://stackoverflow.com/questions/5392344/sending-multipart-formdata-with-jquery-ajax
@@ -66,30 +83,3 @@ if($('#itemName').val()==""||$('#itemPreview').val()==""||$('#itemDescr').val()=
 
   });*/
 }
-
-$(document).ready(function (){
-
-  $.ajax( {
-    url: 'controller/getCategoriesController.php',
-    type: 'post',
-    success: function(output) {
-      json = JSON.parse(output);
-      //console.log(json);
-
-      for(i=0; json.length > i; i++){
-
-        var tmpl = $('#catTempl').clone();
-        tmpl.removeAttr('id');
-        tmpl.html(json[i].name);
-        tmpl.attr('value', json[i].name);
-        $('#selectCategory').append(tmpl);
-      }
-
-
-
-
-    }
-
-  });
-  checkLoginState();
-});
