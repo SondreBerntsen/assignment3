f89e9493b9787479ff09db8cd8bba7bc73604191
mads <mmoigaar@stud.ntnu.no>
2018-06-07 22:48:29 +0200

    satan

--- controller/uploadItemController.php
+++ controller/uploadItemController.php
@@ -1,27 +1,34 @@
 <?php
-require_once('../model/Model.php');
 require_once('../model/Item.php');
-require_once('../model/User.php');
+require_once('../model/Category.php');
 require_once('Controller.php');
 
 class uploadItemController extends Controller{
 
   public function uploadItem(){
-  $user = new User();
-
-  // storing the variables into the array
-  $values = array(
-    'name' => $_POST['name'], 'prev'=> $_POST['preview'],
-    'descr'=> $_POST['descr'], 'ftmp' => $_FILES['imgfile']['tmp_name'],
-    'category' => $_POST['category'], 'id'=>$_SESSION['userID']
-  );
-  // instance of the class Item
-  $item = new Item();
-  // fetches the lastInsertId from the method 'newItem'
-  $itemID = $item->newItem($values);
-
+    $values = array('name' => $_POST['name'], 'prev'=> $_POST['preview'],
+    'descr'=> $_POST['descr'], 'ftmp' => $_FILES['imgfile']['tmp_name'],  'category' => $_POST['category'], 'id'=>$_SESSION['userID']);
+    print_r($values);
+    $item = new Item();
+    //$item-> newItem($values);
+    $itemID = $item->newItem($values); // fetches the lastInsertId from the method 'uploadVideo'
+  }
+  public function getCategories(){
+    $category = new Category();
+    $category->listCategories();
   }
 }
 
-$uploadItem = new uploadItemController ();
-$uploadItem -> uploadItem();
+$controller = new uploadItemController ();
+if(isset($_POST['action'])) {
+  $action = $_POST['action'];
+
+  switch($action) {
+    case 'uploadItem':
+      $controller->uploadItem();
+      break;
+    case 'listCategories':
+      $controller->getCategories();
+      break;
+  }
+}
